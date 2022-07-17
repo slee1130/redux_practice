48ad3542139e58ef657163d4b016cb5e6c643667
Alice Lee <alicesmlee91@gmail.com>
2022-07-16 21:46:04 -0700

    feat: todos container & counter container

---
+++ src/components/Todos.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const TodoItem = React.memo(function TodoItem({ todo, onToggle }) {
+  return (
+    <li
+      style={{ textDecoration: todo.done ? "line-through" : "none" }}
+      onClick={() => onToggle(todo.id)}
+    >
+      {todo.text}
+    </li>
+  );
+});
+
+const TodoList = React.memo(function TodoList({ todos, onToggle }) {
+  return (
+    <li>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} onToggle={onToggle} />
+      ))}
+    </li>
+  );
+});
+
+function Todos({ todos, onCreate, onToggle }) {
+  const [text, setText] = useState("");
+  const onChange = (e) => setText(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault(); //새로고침 방지
+    onCreate(text);
+    setText("");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={text}
+          onChange={onChange}
+          placeholder="please enter your todo"
+        />
+        <button type="submit">register</button>
+      </form>
+      <TodoList todos={todos} onToggle={onToggle} />
+    </div>
+  );
+}
+
+export default React.memo(Todos);
